9be63a287c33861b99fddccab0ee8171ad85f05b
Victor Rocha <xhugo@live.com>
2024-06-24 11:02:09 -0300

    Adicionar arquivos de projeto.

---
+++ BackendChallenge.Api/Models/Entity/OrderEntity.cs
@@ -0,0 +1,15 @@
+﻿namespace BackendChallenge.Api.Models.Entity
+{
+    public class OrderEntity
+    {
+        public int OrderId { get; set; }
+
+        public int ClientId { get; set; }
+
+        public double Total { get; set; }
+
+        public List<OrderItem> Itens { get; set; }
+
+
+    }
+}
---
+++ BackendChallenge.Api/Models/Entity/OrderItem.cs
@@ -0,0 +1,27 @@
+﻿namespace BackendChallenge.Api.Models.Entity
+{
+    public class OrderItem
+    {
+
+        public string Product { get; set; }
+
+        public int Quantity { get; set; }
+
+        public int Price { get; set; }
+
+        public OrderItem(string product, int quantity, int price)
+        {
+            Product = product;
+            Quantity = quantity;
+            Price = price;
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj is OrderItem item &&
+                   Product == item.Product &&
+                   Quantity == item.Quantity &&
+                   Price == item.Price;
+        }
+    }
+}
